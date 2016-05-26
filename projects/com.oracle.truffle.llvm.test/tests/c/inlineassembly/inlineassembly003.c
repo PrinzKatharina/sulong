59d0a3852a5cfd2b97800e1f149660d52a61bb58
PrinzKatharina <kaykay.sunny@gmx.at>
2016-05-27 00:57:23 +0200

    Fix formatting test cases attempt 2

--- projects/com.oracle.truffle.llvm.test/tests/c/inlineassembly/inlineassembly003.c
+++ projects/com.oracle.truffle.llvm.test/tests/c/inlineassembly/inlineassembly003.c
@@ -1,6 +1,6 @@
 int main() {
-	int arg1 = 0;
-	int not = 0;
-	__asm__ ( "notl %%eax;" : "=a" (not) : "a" (arg1) );
-	return not;
+  int arg1 = 0;
+  int not = 0;
+  __asm__("notl %%eax;" : "=a"(not) : "a"(arg1));
+  return not;
 }
