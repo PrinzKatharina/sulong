1a5945a2af4ecc821fd6a694f153e0b5847aa4d6
PrinzKatharina <kaykay.sunny@gmx.at>
2016-05-27 00:37:32 +0200

    Fix formatting test cases

--- projects/com.oracle.truffle.llvm.test/tests/c/inlineassembly/inlineassembly002.c
+++ projects/com.oracle.truffle.llvm.test/tests/c/inlineassembly/inlineassembly002.c
@@ -2,8 +2,6 @@ int main() {
 	int arg1 = 44;
 	int arg2 = 2;
 	int sub = 0;
-	__asm__ ( "subl %%ebx, %%eax;"
-        : "=a" (sub)
-        : "a" (arg1), "b" (arg2) );
+	__asm__ ( "subl %%ebx, %%eax;" : "=a" (sub) : "a" (arg1), "b" (arg2) );
 	return sub;
 }
